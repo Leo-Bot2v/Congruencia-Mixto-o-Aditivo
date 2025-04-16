7c5cb612e23f9e4450a08328c5222e4ea14d33b0
Leo-Bot2v <gremingerlg2@gmail.com>
2025-04-16 16:49:46 -0400

    Forzar reindexación

---
+++ Pruebas/Main.py
@@ -0,0 +1,67 @@
+import sys
+import os
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtCore import QUrl, Qt
+from PySide6.QtGui import QIcon
+from static.Backend import Backend
+from PySide6.QtWebChannel import QWebChannel
+from static.Interceptor import Interceptor
+
+os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu-process-crash-reporting --enable-webgl --ignore-gpu-blacklist"
+
+if getattr(sys, 'frozen', False):
+    ruta_base = sys._MEIPASS
+else:
+    ruta_base = os.path.dirname(os.path.abspath(__file__))
+
+ruta_res = os.path.join(ruta_base, "icono.res")
+
+# Clase del navegador PySide6
+class MiNavegador(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Congruencia Mixto o Aditivo")
+        self.setWindowIcon(QIcon(ruta_res.replace('\\', '/')))
+
+        self.webview = QWebEngineView()
+        self.webview.setUrl(QUrl("file:///templates/index.html"))
+
+        # Agregar el interceptor de URLs
+        self.interceptor = Interceptor(self.webview)
+        self.webview.page().profile().setUrlRequestInterceptor(self.interceptor)
+
+        # Crear un Backend para la comunicación
+        self.backend = Backend()
+
+        # Crear un QWebChannel
+        self.channel = QWebChannel()
+        self.channel.registerObject("backend", self.backend)
+
+        # Asignar el canal a la página web
+        self.webview.page().setWebChannel(self.channel)
+
+        # Desactivar el menú contextual
+        self.webview.setContextMenuPolicy(Qt.NoContextMenu)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.webview)
+        self.setLayout(layout)
+
+        self.setMinimumSize(1366, 715)
+        self.showMaximized()
+
+        self.webview.loadFinished.connect(self.recargar_pagina)
+        self.reloaded = False
+
+    def recargar_pagina(self, ok):
+        if ok and not self.reloaded:
+            self.reloaded = True
+            self.webview.reload()
+
+# Iniciar la aplicación PySide6
+app = QApplication(sys.argv)
+ventana = MiNavegador()
+ventana.show()
+app.exec()
